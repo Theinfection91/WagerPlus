bcbc35a5c2d7f6fe30715ef474babe6fdeed62b2
Chase Carter <149976513+Theinfection91@users.noreply.github.com>
2025-01-26 17:50:04 -0500

    Further structuring of projects and folders and adding placeholder files for now.

---
+++ src/WagerPlus.Bot/SlashCommands/TestCommands.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WagerPlus.Core.Models;
+
+namespace WagerPlus.Bot.SlashCommands
+{
+    public class TestCommands
+    {
+
+    }
+}
