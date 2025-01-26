7ef85d3330de37a5093da2986ed207edb666c24e
Chase Carter <149976513+Theinfection91@users.noreply.github.com>
2025-01-26 10:45:43 -0500

    Starting ground work for Program.cs to get base bot up and running.

--- WagerBot/Program.cs
+++ WagerBot/Program.cs
@@ -1,7 +1,31 @@
-﻿internal class Program
+﻿using System;
+using System.Reflection;
+using System.Threading.Tasks;
+using Discord;
+using Discord.Commands;
+using Discord.Interactions;
+using Discord.WebSocket;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+
+namespace WagerBot
 {
-    private static void Main(string[] args)
+    public class Program
     {
-        Console.WriteLine("Hello, World!");
+        private DiscordSocketClient _client;
+        private CommandService _commands;
+        private IServiceProvider _services;
+        private InteractionService _interactionService;
+
+        public static async void Main(string[] args)
+        {
+            var program = new Program();
+            await program.RunAsync();
+        }
+
+        public async Task RunAsync()
+        {
+
+        }
     }
 }
